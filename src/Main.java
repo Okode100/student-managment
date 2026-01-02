5e523bde4704b7228f1dd50df7b9502cb573b187
Moses Okode <mosesokode76@gmail.com>
2026-01-03 00:47:58 +0300

    refactor the constuctor lieup parameters

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,6 @@ public static void main(String[] args){
     StudentController controller = new StudentController();
     controller.createStudent("Moses Okode", 22, 101, "mosesokode76@gmail.com");
 
+    System.out.println(controller);
+
 }
